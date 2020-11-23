a8a4a0849cc69a98313a5eafd9825464466a171a
Filip Rashev <frashev@gmail.com>
2020-11-23 20:34:19 +0200

    First FT specced out in comments, and now uses unittest.

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,35 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox() # Starting a Selenium "webdriver"
-browser.get('http://localhost:8000') # Using it to open up a web page
+class NewVisitorTest(unittest.TestCase):
 
-assert 'Django' in browser.title # Checking(making a test assertion)
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # She goes to check out its homepage
+        self.browser.get('http://localhost:8000')
+
+        # She notives the page title and header mention to-do lists
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!')
+
+        # She is invited to enter a to-do item straight away
+
+# this clause -- this is how a Python script checks
+# if it has been executed from the command line
+# We call unittest.main(), which launches the unittest test runner
+if __name__ == '__main__':
+        unittest.main()
+
+
+
+
+
+
+
+
+
