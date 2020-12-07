dcd12e33856c354ccf54ab36c52efe50cb6b936a
Filip Rashev <frashev@gmail.com>
2020-12-07 10:41:27 +0200

    Redirect after POST, and show all items in template

--- lists/views.py
+++ lists/views.py
@@ -1,11 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
+from lists.models import Item
 
 # Create your views here.
 # Instead of building our own HttpsResponse, we now use the Django render
 # function. It takes the request as its first parameter and the name of the
 # template to render.
 def home_page(request):
-    return render(request, 'home.html', {
-        'new_item_text': request.POST.get('item_text', ''),
-    })
+    if request.method == 'POST':
+        Item.objects.create(text=request.POST['item_text'])
+        return redirect('/')
+
+    items = Item.objects.all()
+    return render(request, 'home.html', {'items': items})
