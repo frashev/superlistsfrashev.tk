d41fbb02ddf141d57e25276da3a3776aef9dedc2
Filip Rashev <frashev@gmail.com>
2020-12-16 19:07:13 +0200

    Moved Fts into their own individual files

---
+++ functional_tests/base.py
@@ -0,0 +1,40 @@
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from selenium import webdriver
+from selenium.common.exceptions import WebDriverException
+import time
+import os
+
+MAX_WAIT = 10
+
+class FunctionalTest(StaticLiveServerTestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        staging_server = os.environ.get('STAGING_SERVER')
+        if staging_server:
+            self.live_server_url = 'http://' + staging_server
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element_by_id('id_list_table')
+                rows = table.find_elements_by_tag_name('tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
+
+# These lines check whether the Python script has been executed
+# from the command line or just imported by another script.
+# We call unittest.main(), which launches the unittest test runner
+# which will automatically find test casses and methods in the file
+# and run them
+
+# if __name__ == '__main__':
+#     unittest.main(warnings='ignore')
