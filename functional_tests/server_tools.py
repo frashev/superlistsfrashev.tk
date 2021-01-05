7f9a2ab74d55d01820fc41b29f0304ce0d7b4ba7
Filip Rashev <frashev@gmail.com>
2021-01-05 12:51:20 +0200

    updated server_tools.py. Bug.

--- functional_tests/server_tools.py
+++ functional_tests/server_tools.py
@@ -6,7 +6,7 @@ def _get_manage_dot_py(host):
 
 def reset_database(host):
     manage_dot_py = _get_manage_dot_py(host)
-    with settings(host_string=f'frashev{host}'):
+    with settings(host_string=f'frashev@{host}'):
         run(f'{manage_dot_py} flush --noinput')
 
 def create_session_on_server(host, email):
